1958fbf281e3b50afa9537d5891fc9d5b2a2acb7
letuanhai <letuanhai@live.com>
2021-01-02 09:34:52 +0700

    Solve step 3 with different strategy

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,4 +1,3 @@
-# write your code here
 cells = input("Enter cells:")
 c = list(cells)
 print("---------")
@@ -7,39 +6,28 @@ print("|", " ".join(c[3:6]), "|", sep=" ")
 print("|", " ".join(c[6:]), "|", sep=" ")
 print("---------")
 
-three_x_in_row = (
-    (c[0] == c[1] == c[2] == "X")
-    or (c[0] == c[3] == c[6] == "X")
-    or (c[0] == c[4] == c[8] == "X")
-    or (c[2] == c[4] == c[6] == "X")
-    or (c[2] == c[4] == c[6] == "X")
-    or (c[2] == c[5] == c[8] == "X")
-    or (c[6] == c[7] == c[8] == "X")
-    or (c[1] == c[4] == c[7] == "X")
-    or (c[3] == c[4] == c[5] == "X")
-)
-print(three_x_in_row)
-three_o_in_row = (
-    (c[0] == c[1] == c[2] == "O")
-    or (c[0] == c[3] == c[6] == "O")
-    or (c[0] == c[4] == c[8] == "O")
-    or (c[2] == c[4] == c[6] == "O")
-    or (c[2] == c[4] == c[6] == "O")
-    or (c[2] == c[5] == c[8] == "O")
-    or (c[6] == c[7] == c[8] == "O")
-    or (c[1] == c[4] == c[7] == "O")
-    or (c[3] == c[4] == c[5] == "O")
-)
-print(three_o_in_row)
+# Make an array of possible winning positions
+win_pos = [
+    c[0] + c[1] + c[2],
+    c[0] + c[3] + c[6],
+    c[0] + c[4] + c[8],
+    c[2] + c[4] + c[6],
+    c[2] + c[4] + c[6],
+    c[2] + c[5] + c[8],
+    c[6] + c[7] + c[8],
+    c[1] + c[4] + c[7],
+    c[3] + c[4] + c[5],
+]
+
 empty_cell = "_" in c
 count_x = c.count("X")
 count_o = c.count("O")
 
-if (three_o_in_row and three_x_in_row) or abs(count_x - count_o) > 1:
+if ("XXX" in win_pos and "OOO" in win_pos) or abs(count_x - count_o) > 1:
     print("Impossible")
-elif three_o_in_row:
+elif "OOO" in win_pos:
     print("O wins")
-elif three_x_in_row:
+elif "XXX" in win_pos:
     print("X wins")
 elif empty_cell:
     print("Game not finished")
